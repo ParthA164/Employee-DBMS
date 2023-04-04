574980bd9c7b178d402363efd369c8212dc9c196
ParthA164 <128605493+ParthA164@users.noreply.github.com>
2023-04-04 23:10:11 +0530

    Add files via upload

---
+++ Data generator.py
@@ -0,0 +1,22 @@
+from random import *
+import mysql.connector as mydb
+
+mycon = mydb.connect(host='localhost',user='root',password="41710260@@2963//",database="company")
+mycur = mycon.cursor()
+
+s=("Marlo Ramaswamy,Yashraj Gandhi,Arjun Bose,Rakhi Thakkar,Bimla Bandi,Prabha Mehta,Nilima Amin,Zara Palan,Jagdish Divan,Ibrahim Chacko")
+l=s.split(",")
+
+for i in range(1,11):
+  x=randint(20,35)*1000
+  k=randint(0,2)
+  d=["PYTHON","MYSQL","TKINTER"]
+  y=d[k]
+  print(x,y)
+  n=l[i-1]
+  mycur.execute("insert into emp values("+ str(i) +",'"+ n +"','"+ y +"',"+ str(x) +")")
+  mycur.execute("commit")
+
+
+    
+  
